19b368aa7f4ea23ed51eab581a87fd60678f055a
Alex Platteeuw <aplatteeuw@outlook.fr>
2020-05-07 19:13:23 +0200

    exercise update

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,40 @@
+require 'json'
+require 'open-uri'
+
+class PagesController < ApplicationController
+  def new
+    @letters = Array.new(8) { ('a'..'z').to_a.sample }
+  end
+
+  def score
+    @attempt = params[:attempt]
+    @grid = params[:grid]
+    @result = test_word
+  end
+
+  private
+
+  def test_word
+    if !made_out_of_grid?
+      session[:score] = 0
+      "Sorry but #{@attempt.upcase} can't be build out of #{@grid.upcase.chars.join(', ')}"
+    elsif !valid_english_word?
+      session[:score] = 0
+      "Sorry but #{@attempt.upcase} does not seem to be a valid English word"
+    else
+      session[:score].blank? ? session[:score] = @attempt.length : session[:score] += @attempt.length
+      "Congratulations! #{@attempt.upcase} is a valid English word!"
+    end
+  end
+
+  def made_out_of_grid?
+    @attempt.chars - @grid.chars == []
+  end
+
+  def valid_english_word?
+    url = "https://wagon-dictionary.herokuapp.com/#{@attempt}"
+    attempt_serialized = open(url).read
+    JSON.parse(attempt_serialized)["found"]
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'new', to: 'pages#new'
+  post 'score', to: 'pages#score'
+  root to: 'pages#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
