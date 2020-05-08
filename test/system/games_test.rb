ab1df796b42ea127ece3844d0d5d3cbe59256df0
Alex Platteeuw <aplatteeuw@outlook.fr>
2020-05-08 09:02:05 +0200

    adding some more tests

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -8,9 +8,11 @@ class GamesTest < ApplicationSystemTestCase
   end
   test "fill the form with a random word, click the play button, and get a message that the word is not in the grid.
 " do
-  visit new_url
-  fill_in("attempt", with: Array.new(10) { ('a'..'z').to_a.sample }.join)
-  click_on("Play")
-  page.has_content?("can't be build out of")
+    visit new_url
+    fill_in("attempt", with: Array.new(10) { ('a'..'z').to_a.sample }.join)
+    click_on("Play")
+    page.has_content?("can't be build out of")
   end
+  test "fill the form with a one-letter consonant word, click play, and get a message it’s not a valid English word"
+  fill_in("attempt", with: Array.new(1) { ('a'..'z').to_a.sample }.join)
 end
